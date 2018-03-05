441552da5715d5bf0619e031306cbc1d2420cafd
dandyxu <dandyjefferson@gmail.com>
2018-03-05 18:01:29 +0000

    Day 1: Familiar with WPPB Boilerplate and follow tutorials to set up an Option Page

--- admin/class-advanced-display-id-admin.php
+++ admin/class-advanced-display-id-admin.php
@@ -52,6 +52,30 @@ class Advanced_Display_Id_Admin {
 		$this->plugin_name = $plugin_name;
 		$this->version = $version;
 
+		add_action( 'admin_init', array( $this, 'custom_objects' ) );
+		
+		// For Post Management
+		add_filter( 'manage_posts_columns', array( $this, 'add_column' ) );
+		add_action( 'manage_posts_custom_column', array( $this, 'add_value' ), 10, 2 );
+		// For Page Management
+		add_filter( 'manage_pages_columns', array( $this, 'add_column' ) );
+		add_action( 'manage_pages_custom_column', array( $this, 'add_value' ), 10, 2 );
+		// For Media Management
+		add_filter( 'manage_media_columns', array( $this, 'add_column' ) );
+		add_action( 'manage_media_custom_column', array( $this, 'add_value' ), 10, 2 );
+		// For Link Management
+		add_filter( 'manage_link-manager_columns', array( $this, 'add_column' ) );
+		add_action( 'manage_link_custom_column', array( $this, 'add_value' ), 10, 2 );
+		// For Category Management
+		add_action( 'manage_edit-link-categories_columns', array( $this, 'add_column' ) );
+		add_filter( 'manage_link_categories_custom_column', array( $this, 'add_return_value' ), 10, 3 );
+		// For User Management
+		add_action( 'manage_users_columns', array( $this, 'add_column' ) );
+		add_filter( 'manage_users_custom_column', array( $this, 'add_return_value' ), 10, 3 );
+		// For Comment Management
+		add_action( 'manage_edit-comments_columns', array( $this, 'add_column' ) );
+		add_action( 'manage_comments_custom_column', array( $this, 'add_value' ), 10, 2 );
+
 	}
 
 	/**
@@ -100,4 +124,124 @@ class Advanced_Display_Id_Admin {
 
 	}
 
+	/**
+	 * Register the administration menu for this plugin into the WordPress Dashboard menu.
+	 *
+	 * @since    1.0.0
+	 */
+	public function add_plugin_admin_menu() {
+		/**
+		 * Add a settings page for this plugin to the Settings menu.
+		 *
+		 * NOTE:  Alternative menu locations are available via WordPress administration menu functions.
+		 *
+		 *        Administration Menus: http://codex.wordpress.org/Administration_Menus
+		 *
+		 * add_options_page( $page_title, $menu_title, $capability, $menu_slug, $function);
+		 *
+		 * @link https://codex.wordpress.org/Function_Reference/add_options_page
+		 */
+		add_submenu_page( 'tools.php', 'Advanced Display IDs Option Page', 'Display IDs Options', 'manage_options', $this->plugin_name, array($this, 'display_plugin_setup_page')
+		);
+
+	}
+
+	/**
+	 * Add settings action link to the plugins page.
+	 *
+	 * @since    1.0.0
+	 */
+	public function add_action_links( $links ) {
+		/*
+		*  Documentation : https://codex.wordpress.org/Plugin_API/Filter_Reference/plugin_action_links_(plugin_file_name)
+		*/
+	$settings_link = array(
+		'<a href="' . admin_url( 'tools.php?page=' . $this->plugin_name ) . '">' . __( 'Settings', $this->plugin_name ) . '</a>',
+	);
+	return array_merge(  $settings_link, $links );
+	}
+	/**
+	 * Render the settings page for this plugin.
+	 *
+	 * @since    1.0.0
+	 */
+	public function display_plugin_setup_page() {
+		include_once( 'partials/' . $this->plugin_name . '-admin-display.php' );
+	}
+	/**
+	 * Validate fields from admin area plugin settings form ('exopite-lazy-load-xt-admin-display.php')
+	 * @param  mixed $input as field form settings form
+	 * @return mixed as validated fields
+	 */
+	public function validate($input) {
+		$valid = array();
+		$valid['example_checkbox'] = ( isset( $input['example_checkbox'] ) && ! empty( $input['example_checkbox'] ) ) ? 1 : 0;
+		$valid['example_text'] = ( isset( $input['example_text'] ) && ! empty( $input['example_text'] ) ) ? esc_attr( $input['example_text'] ) : 'default';
+		$valid['example_select'] = ( isset($input['example_select'] ) && ! empty( $input['example_select'] ) ) ? esc_attr($input['example_select']) : 1;
+		return $valid;
+	}
+	public function options_update() {
+		register_setting( $this->plugin_name, $this->plugin_name, array( $this, 'validate' ) );
+	}
+
+	/**
+	 * Hooks to the 'admin_init'
+	 *
+	 * @return void
+	 */
+	public function custom_objects() {
+		// For Custom Taxonomies
+		$taxonomies = get_taxonomies( array( 'public' => true ), 'names' );
+		foreach ( $taxonomies as $custom_taxonomy ) {
+			if ( isset( $custom_taxonomy ) ) {
+				add_action( 'manage_edit-' . $custom_taxonomy . '_columns', array( $this, 'add_column' ) );
+				add_filter( 'manage_' . $custom_taxonomy . '_custom_column', array( $this, 'add_return_value' ), 10, 3 );
+			}
+		}
+		// For Custom Post Types
+		$post_types = get_post_types( array( 'public' => true ), 'names' );
+		foreach ( $post_types as $post_type ) {
+			if ( isset( $post_type ) ) {
+				add_action( 'manage_edit-' . $post_type . '_columns', array( $this, 'add_column' ) );
+				add_filter( 'manage_' . $post_type . '_custom_column', array( $this, 'add_return_value' ), 10, 3 );
+			}
+		}
+	}
+
+	/**
+	 * Adds column to edit screen
+	 *
+	 * @param mixed $cols
+	 * @return void
+	 */
+	public function add_column( $cols ) {
+		$cols['advanced-display-id'] = 'ID';
+		return $cols;
+	}
+	/**
+	 * Adds id value
+	 *
+	 * @param mixed $column_name
+	 * @param mixed $id
+	 * @return void
+	 */
+	public function add_value( $column_name, $id ) {
+		if ( 'advanced-display-id' === $column_name ) {
+			echo $id;
+		}
+	}
+	/**
+	 * Adds id value
+	 *
+	 * @param mixed $value
+	 * @param mixed $column_name
+	 * @param mixed $id
+	 * @return void
+	 */
+	public function add_return_value( $value, $column_name, $id ) {
+		if ( 'advanced-display-id' === $column_name ) {
+			$value = $id;
+		}
+		return $value;
+	}
 }
--- admin/partials/advanced-display-id-admin-display.php
+++ admin/partials/advanced-display-id-admin-display.php
@@ -11,6 +11,67 @@
  * @package    Advanced_Display_Id
  * @subpackage Advanced_Display_Id/admin/partials
  */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) die;
 ?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
+<div class="wrap">
+    <h2>Welcome to Display Advanced IDs Options Page <?php _e(' Options ', $this->plugin_name); ?></h2>
+    <form method="post" name="cleanup_options" action="options.php">
+    <?php
+        //Grab all options
+        $options = get_option($this->plugin_name);
+        $example_select = ( isset( $options['example_checkbox'] ) && ! empty( $options['example_checkbox'] ) ) ? esc_attr( $options['example_checkbox'] ) : '1';
+        $example_text = ( isset( $options['example_text'] ) && ! empty( $options['example_text'] ) ) ? esc_attr( $options['example_text'] ) : 'default';
+        $example_radio = ( isset( $options['example_radio'] ) && ! empty( $options['example_radio'] ) ) ? 1 : 0;
+        settings_fields($this->plugin_name);
+        do_settings_sections($this->plugin_name);
+        // Sources: - http://searchengineland.com/tested-googlebot-crawls-javascript-heres-learned-220157
+        //          - http://dinbror.dk/blog/lazy-load-images-seo-problem/
+        //          - https://webmasters.googleblog.com/2015/10/deprecating-our-ajax-crawling-scheme.html
+    ?>
+
+    <!-- Select -->
+    <!-- <fieldset>
+        <p><?php //_e( 'Example Select.', $this->plugin_name ); ?></p>
+        <legend class="screen-reader-text">
+            <span><?php //_e( 'Example Select', $this->plugin_name ); ?></span>
+        </legend>
+        <label for="example_select">
+            <select name="<?php //echo $this->plugin_name; ?>[example_select]" id="<?php //echo $this->plugin_name; ?>-example_select">
+                <option <?php //if ( $example_select == 'first' ) echo 'selected="selected"'; ?> value="first">First</option>
+                <option <?php //if ( $example_select == 'second' ) echo 'selected="selected"'; ?> value="second">Second</option>
+            </select>
+        </label>
+    </fieldset> -->
+
+    <!-- Text -->
+    <!-- <fieldset>
+        <p><?php //_e( 'Example Text.', $this->plugin_name ); ?></p>
+        <legend class="screen-reader-text">
+            <span><?php //_e( 'Example Text', $this->plugin_name ); ?></span>
+        </legend>
+        <input type="text" class="example_text" id="<?php //echo $this->plugin_name; ?>-example_text" name="<?php //echo $this->plugin_name; ?>[example_text]" value="<?php //if( ! empty( $example_text ) ) echo $example_text; else echo 'default'; ?>"/>
+    </fieldset> -->
+
+    <!-- Radio Button -->
+    <fieldset>
+        <h2><?php _e( 'Overall Setting', $this->plugin_name ); ?></h2>
+        <!-- <legend class="example-radio">
+            <span><?php //_e( 'Post ID management', $this->plugin_name ); ?></span>
+        </legend> -->
+        <label for="<?php echo $this->plugin_name; ?>-overall_radio_show">
+            <input type="radio" id="<?php echo $this->plugin_name; ?>-overall_radio_show" name="<?php echo $this->plugin_name; ?>[example_radio]" value="1" <?php checked( $example_radio, 1 ); ?> />
+            <span><?php esc_attr_e('Show', $this->plugin_name); ?></span>
+        </label>
+        <label for="<?php echo $this->plugin_name; ?>-overall_radio_hide">
+            <input type="radio" id="<?php echo $this->plugin_name; ?>-overall_radio_hide" name="<?php echo $this->plugin_name; ?>[example_radio]" value="2" <?php checked( $example_radio, 1 ); ?> />
+            <span><?php esc_attr_e('Hide', $this->plugin_name); ?></span>
+        </label>
+    </fieldset>
+
+    <?php submit_button( __( 'Save all changes', $this->plugin_name ), 'primary','submit', TRUE ); ?>
+    </form>
+
